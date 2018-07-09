467746f0b844ecc80217f0a6a88800b50ee27257
Englada <38507630+Englada@users.noreply.github.com>
2018-07-09 11:51:13 -0400

    Made a matrix library

---
+++ matrix.py
@@ -0,0 +1,19 @@
+from random import *
+# M1=[[5,3],[7,5]]
+
+# M2=[[6,4],[9,4]]
+
+# x = [[i for i in range(0,2)] for i in range(0,10)]
+
+# def add(M1):
+#     for i in :
+#     	print i
+#     	for j in M1:
+#     		print j
+#     		c = i + j
+#     return c
+
+# print(add(M1,M2))
+
+x = [[i for i in range(1,randint(0,10))] for i in range(1,randint(0,10))]
+print x
